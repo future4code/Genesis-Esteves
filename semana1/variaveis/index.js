4bc5bf612d013c12f0b4cb314e64c88bc38c01a3
Genesisferreira <genesisferreira2010@hotmail.com>
2021-05-03 16:21:53 -0300

    exercicios de variaveis

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,103 @@
+/*
+const nome="genesis"
+const sobrenome="esteves"
+let idade= "31"
+console.log("oi meu nome e", nome, sobrenome, "tenho", idade);*/
+
+/*const nome = prompt("user qual seu nome")
+
+let idade = prompt("qual sua idade")
+
+console.log("o tipo da variavelnome", typeof nome)
+
+console.log("o tipo da variavelnome", typeof idade)*/
+
+/*let a = 10
+let b = 10
+
+console.log(b)
+
+b=5
+console.log(a, b)*/
+
+/*let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)*/
+
+/*let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+let valorDaDiaria = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${horasTrabalhadas/valorDaDiaria} por hora`)*/
+
+
+
+//Exercícios//
+
+//1//
+
+/*const nome = prompt("qual seu nome")
+
+let idade = prompt("qual sua idade")
+
+console.log("nome", typeof nome)
+
+console.log("idade", typeof Number)*/
+
+/*nome string
+ idade function
+ ainda estou meio perdido mas foi este o retorno*/
+
+ /*console.log("Ola", nome, "você tem", idade);*/
+
+/*retorno 
+nome string
+idade function
+Ola genesisferreira você tem 31*/
+
+//2//
+
+/*const dificuldades = prompt("você esta com dificuldades em javaScrit ? ")
+
+const variaveis = prompt("entendeu as variaveis ? ")
+
+const entendeuTudo = prompt("entendeu tudo ? ")
+
+console.log("você esta com dificuldades em javaScrit ?", dificuldades)
+
+console.log("entendeu as variaveis ?", variaveis)
+
+console.log("entendeu tudo ? ", entendeuTudo)*/
+
+/* retorno
+você esta com dificuldades em javaScrit ? sim
+entendeu as variaveis ? nao
+entendeu tudo ?  nao */
+ 
+//3//
+
+
+
+
+//desafio//
+let a = prompt("soma")
+let b = prompt("soma")
+alert(a+b)
+
+console.log("a", typeof Number)
+console.log("b", typeof Number)
+
+
+
+
+
+
+ 
+ 
+
+
+
+
+
