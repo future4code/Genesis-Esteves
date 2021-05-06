35cee42fee720e062433c8ca4f98b7b91bf7ec3d
Genesisferreira <genesisferreira2010@hotmail.com>
2021-05-06 16:31:16 -0300

    funçoes

---
+++ semana1/funçoes/index.js
@@ -0,0 +1,66 @@
+/*-------------------->Exercícios de escrita de código<--------------------
+
+1) Escreva as funções explicadas abaixo:
+
+a) A função não deve receber nenhum parâmetro e deve imprimir uma mensagem 
+falando algumas informações sobre você, como:
+
+"Eu sou Caio, tenho 23 anos, moro em São Paulo e sou estudante."
+
+Troque o nome, idade, cidade e se é estudante ou não por informações sobre você. 
+Lembrando que a função não possui entradas, apenas imprime essa mensagem.
+
+
+
+
+
+
+*/
+
+ /* AQUI PESSO DESCULPAS MAS NAO ENTOU AINDA NA CABEÇA BUGUEI MUITO 
+ ATE AQUI MEU PIOR MOMENTO FICO MUITO CHATEADO POR NAO ENTREGAR 
+ OM O RESULTADO ENTREGADO VOU CORRER ATRAZ E CORRIGIR ISTO 
+  NOVAMENTE DESCULPAS   */
+
+/*const imprimeOlaNome = function () {
+    const inicio = (`Eu sou ${nome} tenhome, idade, cidade, estud ${idade} anos,
+    moro em ${cidade} e ${estudo} estudante`)
+
+
+
+
+    const nome = genesis
+    const idade = 31
+    const cidade = minas 
+    const estudo = sou 
+
+    return im
+
+    console.log(imprimeOlaNome)
+}
+*/
+
+
+/*function imprimeOlaNome () {
+      console.log(`Eu sou ${nome} tenho ${idade} anos,
+      moro em ${cidade} e ${vcEstuda} estudante`)
+
+    const nome = nome
+    const idade = idade
+    const cidade = cidade
+    const vcEstuda = vcEstuda
+    }
+
+    console.log(imprimeOlaNome(nome=genesis))
+    imprimeOlaNome ()
+    imprimeOlaNome ()
+    */
+
+
+
+
+
+//const nome = "genesis"
+//const idade = 31
+//const cidade = "minas-gerais"
+//const estudante = "sou-estudante" 
