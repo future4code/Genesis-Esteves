132e91b0d4ec5187a1abde182a59b849fa3be2bd
Genesisferreira <genesisferreira2010@hotmail.com>
2021-06-08 16:51:06 -0300

    boiler-plate1

--- semana6/labenu-forms/src/App.js
+++ semana6/labenu-forms/src/App.js
@@ -4,6 +4,32 @@ import React from "react";
 function App() {
   return (
     <div>
+      <h1>ETAPA 1 - DADOS GERAIS</h1>
+      <form>
+        <div>
+          <label>
+           1. Qual o seu nome?
+          </label>
+          <input type="text" name="" />
+        </div>
+        <div>
+          <label>
+           1. Qual o seu nome?
+          </label>
+          <input type="text" name="" />
+        </div>
+        <div>
+          <label>
+           1. Qual o seu nome?
+          </label>
+          <input type="text" name="" />
+        </div>
+        <div>
+        <input/>
+        </div>
+             
+        <input type="submit" value="proxima etapa" />
+      </form>
     </div>
   );
 }
