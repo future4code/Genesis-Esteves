8137137adbd7ca4eb2d56c3abfef69e422bd5a7e
genesisferreira <genesisferreira2010@hotmail.com>
2021-07-12 07:53:17 -0300

    finalizado

--- semana10/labex/src/page/HomePage/HomePage.jsx
+++ semana10/labex/src/page/HomePage/HomePage.jsx
@@ -1,15 +1,22 @@
-import React from "react"
-import '../../App.css';
+import React from 'react'
+import { Button } from '@material-ui/core';
+import { ButtonsContainer } from './styles';
+import { Link } from 'react-router-dom';
+import PageTitle from '../../components/PageTitle';
 
-export const HomePage = () => {
 
-    return (
-        <div className="App">
-            <h1>LabeX</h1>
-        <button>Ver Viagens</button>
-        <button>Area De Admin</button>
-        </div>
+const HomePage = () => {
+  return <div>
+    <PageTitle title={'LabeX'}/>
+    <ButtonsContainer>
+      <Link to={'/login'}>
+        <Button variant={'outlined'} color={'secondary'}>Área do administrador</Button>
+      </Link>
+      <Link to={'/inscricao'}>
+        <Button variant={'contained'} color={'primary'}>Se cadastre</Button>
+      </Link>
+    </ButtonsContainer>
+  </div>
+}
 
-    );
-
-}; 
+export default HomePage
